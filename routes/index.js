ef02b3a788e70a591fb7e9558aabb4c1ac7fe299
Tim Sandberg <tasandberg@gmail.com>
2015-12-19 23:41:37 -0800

    Toggle leds in toggle endpoint

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var GPIO = require('onoff').Gpio;
 var router = express.Router();
 var os = require('os');
 
@@ -11,13 +12,27 @@ router.get('/', function(req, res, next) {
 });
 
 /* POST a control toggle. */
-var buttons = [
-  false, false, false, false, false, false
+var leds = [
+        new GPIO(4, 'out'),
+        new GPIO(14,'out'),
+        new GPIO(15, 'out'),
+        new GPIO(17, 'out'),
+        new GPIO(18, 'out'),
+        new GPIO(27, 'out'),
+        new GPIO(22, 'out'),
+        new GPIO(24, 'out')
 ];
 
 router.post('/button/:id', function(req, res, next) {
-  var id = req.params.id;
-  var status = buttons[id] = buttons[id] ? false : true;
+  var id = req.params.id, status;
+  if (leds[id].readSync() == 1) {
+    status = 0;
+    leds[id].writeSync(status);
+  } else {
+    status = 1;
+   leds[id].writeSync(status);
+  }
+
   res.json({
     'success': true,
     message: req.params.id + ' button toggled',
